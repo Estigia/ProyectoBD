88dac73e9693110bf1c1e42b5b2735116a73e02d
MalvinJazz <malvinjazz@live.com>
2016-03-26 01:00:00 -0600

    modelos y admin de Registros creados

--- Env/src/Registros/admin.py
+++ Env/src/Registros/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Victima,Registro,Actividad
+from .forms import RegistroForm,VictimaForm
 
 # Register your models here.
+class VictimaAdmin(admin.ModelAdmin):
+	list_display = ["id","__unicode__","edad","profesion","cui","sexo"]
+	form = VictimaForm
+
+class RegistroAdmin(admin.ModelAdmin):
+	list_display = ["id","fecha","descripcion"]
+	form = RegistroForm
+
+admin.site.register(Victima,VictimaAdmin)
+admin.site.register(Registro,RegistroAdmin)
--- Env/src/Registros/models.py
+++ Env/src/Registros/models.py
@@ -3,3 +3,32 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Victima(models.Model):
+	id = models.AutoField(primare_key=True)
+	nombres = models.CharField(max_length=55)
+	apellidos = models.CharField(max_length=55)
+	edad = models.SmallIntegerField()
+	muerto_herido = models.BooleanField(null=None)	
+	profesion = models.CharField(max_length=45, blank=True, null=True)
+	cui = models.CharField(max_length=13, blank=True, null= True)
+	sexo = models.BooleanField(blank=True,null=None)
+
+	def __unicode__(self):
+		return self.nombres," ", self.apellidos
+
+class Registro(models.Model)
+	id = models.AutoField(primare_key=True)
+	fiscal = models.CharField(max_length=60, blank=True, null=True)
+	fecha = models.DateTimeField()
+	descripcion = models.TextField()
+	movil = models.CharField(max_length=100)
+	no_expediente = models.CharField(max_length=40)
+	Localizacion_id = models.ForeignKey('localizaciones.Localizacion', on_delete=models.CASCADE)
+	Arma_id = models.ForeignKey('Armas.Arma', on_delete=models.CASCADE)
+	Victima_id = models.ForeignKey('Victima', on_delete=models.CASCADE)
+
+class Actividad(models.Model)
+	id = models.AutoField(primary_key=True)
+	Registro_id = models.ForeignKey('Registro',on_delete=models.CASCADE)
+	Usuario_id = models.ForeignKey('home.Usuario',on_delete=models.CASCADE)
+	actividad = models.CharField(max_length=45)
