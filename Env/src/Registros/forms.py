ad6cfb6f60baf7e71554d16c191936b200dcd735
Chofo2003 <chofo_03@hotmail.com>
2016-03-26 00:50:58 -0600

    forms y views de la App Registros

---
+++ Env/src/Registros/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from .models import Registro,Victima
+
+class RegistroForm(forms.ModelForm):
+	class Meta:
+		model = Registro
+
+		fields = ["fiscal",
+					"descripcion",
+					"movil",
+					"no_expediente"					
+			]
+
+class VictimaForm(forms.ModelForm):
+	class Meta:
+		model = Victima
+
+		fieldas = ["nombres",
+					"apellidos",
+					"edad",
+					"muerto_herido",
+					"profesion",
+					"cui",
+					"Sexo",
+				]
--- Env/src/Registros/views.py
+++ Env/src/Registros/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .forms import RegistroForm, VictimaForm
 
-# Create your views here.
+def registro(request):
+	form = RegistroForm(request.POST or None)
+	form2 = VictimaForm(request.POST or None)
+
+	context = {
+		"form" :form,
+		"fomr2" : form2,
+		"title": "Registros"
+	}
+
+	if form.is_valid():
+		form.save()
+	if form2.is_valid():
+		form2.save()
+
+	return reder(request,'registros.html',context)
