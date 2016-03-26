480294ad106075aad73f4f07ec5a994ade558ece
23justo <justojul23@hotmail.com>
2016-03-25 21:34:30 -0600

    edicion models,admin en app localizacion

--- Env/src/localizaciones/admin.py
+++ Env/src/localizaciones/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .forms import LocalizacionForm,MunicipioForm,DepartamentoForm
+from .models import Departamento,Municipio,Localizacion
 # Register your models here.
+
+class DepartamentoAdmin(admin.ModelAdmin):
+	list_display = ["id","nombre"]
+	form = DepartamentoForm
+
+class MunicipioAdmin(admin.ModelAdmin):
+	list_display = ["id","nombre"]
+	form = MunicipioForm
+
+class LocalizacionAdmin(admin.ModelAdmin):
+	list_display = ["direccion","tipo"]
+	form = LocalizacionForm
+
+admin.site.register(Departamento,DepartamentoAdmin)
+admin.site.register(Municipio, MunicipioAdmin)
+admin.site.register(Localizacion, LocalizacionAdmin)
--- Env/src/localizaciones/models.py
+++ Env/src/localizaciones/models.py
@@ -3,3 +3,18 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Departamento(models.Model):
+	id = models.AutoField(primary_key = True)
+	nombre = models.CharField(max_length = 45)
+
+class Municipio(models.Model):
+	id = models.AutoField(primary_key = True)
+	nombre = models.CharField(max_length = 45)
+	Departamento_id = models.ForeignKey("Departamento", on_delete = models.CASCADE)
+
+class Localizacion(models.Model):
+	id = models.AutoField(primary_key = True)
+	direccion = models.CharField(max_length = 100)
+	tipo = models.BooleanField(blank=True, null=None)
+	Municipio_id = models.ForeignKey("Municipio", on_delete =  models.CASCADE)
